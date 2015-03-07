7e2c72547302d4c4e358ad0c609c5805464ea30b
buyon2 <rumorscoin2@outlook.com>
2015-03-07 20:57:39 +0100

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1113,7 +1113,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"", ""},
+    {"52.11.186.27", "52.11.186.27"},
     {"", ""},
 };
 
